b37d507af27b3acdde019b55d1db096d3bf9b94b
William Abreu <williamsantos_00@hotmail.com>
2018-07-09 16:25:34 -0300

    comecei a escovar os bits

--- 3-protocol/ack_frame.py
+++ 3-protocol/ack_frame.py
@@ -1,7 +1,13 @@
 class ack_frame:
-    __DELIMITER = 0x7E # 1 byte
-
     def __init__(self, sequence, destination_address, source_address):
-        self.__sequence = sequence # 1 byte
-        self.__destination_address = destination_address # 4 bytes
-        self.__source_address = source_address # 4 bytes
+        self.__bin =  (0x7E << 72) | (sequence << 64) | (destination_address << 32) | (source_address)
+        #             1 byte           1 byte                   4 bytes                  4 bytes
+    
+    def get_sequence(self):
+        return (self.__bin >> 64) & 0xFF
+    
+    def get_destination_addr(self):
+        return (self.__bin >> 32) & 0xFFFFFFFF
+    
+    def get_source_addr(self):
+        return self.__bin & 0xFFFFFFFF
--- 3-protocol/data_frame.py
+++ 3-protocol/data_frame.py
@@ -1,10 +1,46 @@
 class data_frame:
-    __DELIMITER = 0x7E # 1 byte
-
     def __init__(self, sequence, destination_address, source_address, payload):
+        n = len(payload)
+        self.__bin =  \
+            (0x7E << ((11 + n) * 8)) | \
+            (sequence << ((10 + n) * 8)) | \
+            (destination_address << ((6 + n) * 8)) | \
+            (source_address << ((2 + n) * 8)) | \
+            (str_to_bin(payload) << 16) | \
+            (crc(0))
+
         self.__lenght = None # 1 byte
         self.__sequence = sequence # 1 byte
         self.__destination_address = destination_address # 4 bytes
         self.__source_address = source_address # 4 bytes
         self.__payload = payload # n bytes, 0 <= n <= 255
         self.__crc = None # 2 bytes
+
+        # COLOCAR PAYLOAD EM <class 'str'>
+    
+def str_to_bin(payload):
+	return int(bytes(payload, encoding='utf-8').hex(), base=16)
+
+def bin_to_str(payload):
+	return bytearray.fromhex(hex(payload).lstrip('0x')).decode('utf-8')
+
+def crc(frame):
+    return 0
+
+'''
+
+payload : <class 'str'>
+
+str_to_bin..
+
+int(bytes(payload, encoding='utf-8').hex(), base=16)
+
+------------------------------------------------------------
+
+payload : <class 'int'>
+
+bin_to_str..
+
+bytearray.fromhex(hex(payload).lstrip('0x')).decode('utf-8')
+
+'''
