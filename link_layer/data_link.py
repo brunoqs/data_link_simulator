3a9b81d1bbb3d5b4622ba5d153bd2d92acb18b5d
brunoqs <brun_o13@hotmail.com>
2018-07-11 13:55:24 -0300

    tentando juntar as classes

--- link_layer/data_link.py
+++ link_layer/data_link.py
@@ -1,9 +1,19 @@
+from data_frame import *
+from ack_frame import *
+
+import sys
+sys.path.append('../physical_layer/')
+from physical_link import *
+
+from ipaddress import IPv4Address
+
 class data_link:
     def __init__(self):
-        pass
+        self.frame = data_frame(1, IPv4Address("127.0.0.1"), IPv4Address("0.0.0.0"), "oioi")
+        self.physical = physical_link(bin(self.frame.get_frame()))
     
     def Data_Request(self, destination_address, l_sdu):
-        pass
+    	self.physical.F_Data_Request("ala")	
 
     def Data_Indication(self, destination_address, source_address, l_sdu):
-        pass
+    	self.physical.F_Data_Indication("ala")	
--- physical_layer/physical_link.py
+++ physical_layer/physical_link.py
@@ -1,9 +1,38 @@
-class physical_link:
-    def __init__(self):
-        pass
-    
-    def F_Data_Request(self, octeto):
-        pass
+from socket import *
+import random
 
-    def F_Data_Indication(self, octeto):
-        pass
+class physical_link:
+	def __init__(self, bin):
+		self.bin = bin
+	
+	def F_Data_Request(self, octeto):
+		server_name = "127.0.0.1"
+		server_port = 12000
+
+		client_socket = socket(AF_INET, SOCK_STREAM)
+		client_socket.connect((server_name, server_port))
+		
+		lenght_bin = len(self.bin) 
+		for i in range(0,lenght_bin, 4):
+			client_socket.send(self.bin[i:i+4].encode())
+
+		client_socket.close()
+
+	def F_Data_Indication(self, octeto):
+		server_name = "127.0.0.1"
+		server_port = 12000
+
+		server_socket = socket(AF_INET, SOCK_STREAM)
+		server_socket.bind((server_name, server_port))
+		server_socket.listen(1)    
+
+		connection_socket = server_socket.accept()[0]
+
+		frame = ""
+		while len(frame) != len(self.bin):
+			request = connection_socket.recv(1024)
+			frame += request.decode("utf-8")
+
+		print(frame)
+
+		connection_socket.close()
