b7589320d50b225a76ce22c0ff2b60f29d939ca9
William Abreu <williamsantos_00@hotmail.com>
2018-07-15 00:17:53 -0300

    cria varios quadros para l_sdu > 255 pronto

--- link_layer/data_link.py
+++ link_layer/data_link.py
@@ -8,12 +8,47 @@ from ipaddress import IPv4Address
 import time
 
 class data_link:
+	__SEQ_ONE = 0b00000000 # stop-and-wait
+	__SEQ_TWO = 0b10000000 # stop-and-wait
+	__SEQ_UNIQUE_FRAME = 0b00000001 # único quadro
+	__SEQ_LAST_FRAME = 0b10000001 # indica o último quadro enviado
+	__ACK_ONE = 0b00000001
+	__ACK_TWO = 0b10000001
+	__NACK_ONE = 0b00000000
+	__NACK_TWO = 0b10000000
+
 	@staticmethod
 	def Data_Indication(destination_address, source_address, l_sdu):
-		frame = data_frame(1, destination_address, IPv4Address("0.0.0.0"), l_sdu)
 		port_serv = 11000
 		port_client = 12000
 
+		payload_len = len(l_sdu.encode())
+
+		if payload_len <= 255:
+			frame = data_frame(data_link.__SEQ_UNIQUE_FRAME, destination_address, IPv4Address("0.0.0.0"), l_sdu)
+			port_serv, port_client = data_link.__send_frame(frame, destination_address, source_address, port_serv, port_client)
+		else:
+			nframes = (payload_len - 1) // 255 + 1
+			sequence = (data_link.__SEQ_ONE, data_link.__SEQ_TWO)
+			
+			start, end = (0, 255)
+			l_sdu_part = l_sdu.encode()[start:end].decode()
+			frame = data_frame(sequence[0], destination_address, IPv4Address("0.0.0.0"), l_sdu_part)
+			port_serv, port_client = data_link.__send_frame(frame, destination_address, source_address, port_serv, port_client)
+
+			for i in range(1, nframes-1):
+				start, end = (255*i, 255*(i+1))
+				l_sdu_part = l_sdu.encode()[start:end].decode()
+				frame = data_frame(sequence[i%2], destination_address, IPv4Address("0.0.0.0"), l_sdu_part)
+				port_serv, port_client = data_link.__send_frame(frame, destination_address, source_address, port_serv, port_client)	
+			
+			start = 255*(nframes-1)
+			l_sdu_part = l_sdu.encode()[start:].decode()
+			frame = data_frame(data_link.__SEQ_LAST_FRAME, destination_address, IPv4Address("0.0.0.0"), l_sdu_part)
+			port_serv, port_client = data_link.__send_frame(frame, destination_address, source_address, port_serv, port_client)	
+
+	@staticmethod
+	def __send_frame(frame, destination_address, source_address, port_serv, port_client):
 		resend = True
 		while resend: # enquanto receber nack
 			# envia um frame
@@ -39,21 +74,41 @@ class data_link:
 			# abre um ack para ver seu sequence 0=ack 1=nack
 			ack_check = ack_frame(bin_flow_recv=int(ack, base=2)) 
 			n_ack = ack_check.get_sequence()
-			if n_ack == 0b00000001:
+			if n_ack == data_link.__NACK_ONE or n_ack == data_link.__NACK_TWO:
 				resend = True
 			else:
 				resend = False
+			
 			physical_recv.close()
 
 			port_client += 1
 			port_serv += 1
 
+		return port_serv, port_client
+
 	@staticmethod
 	def Data_Request(destination_address, source_address):
-		ack = ack_frame(0, destination_address, source_address)
 		port_serv = 12000
 		port_client = 11000
 
+		sequence = (data_link.__ACK_ONE, data_link.__ACK_TWO)
+		frame, port_serv, port_client = data_link.__recv_frame(sequence[0], destination_address, source_address, port_serv, port_client)
+		l_sdu = data_frame.bin_to_str(frame.get_payload())
+		seq = frame.get_sequence()
+		i = 1
+
+		if seq != data_link.__SEQ_UNIQUE_FRAME:
+			while seq != data_link.__SEQ_LAST_FRAME:
+				frame, port_serv, port_client = data_link.__recv_frame(sequence[i%2], destination_address, source_address, port_serv, port_client)
+				l_sdu += data_frame.bin_to_str(frame.get_payload())
+				seq = frame.get_sequence()
+				i += 1
+		
+		return l_sdu
+
+	@staticmethod
+	def __recv_frame(sequence, destination_address, source_address, port_serv, port_client):
+		ack = ack_frame(sequence, destination_address, source_address)
 		response = True
 		while response: # enquanto tiver que esperar outro pacote
 			# espera o frame
@@ -84,7 +139,10 @@ class data_link:
 				response = False # nao precisa esperar outro frame
 			# crc = False envia ack com sequence = 1 (nack)
 			else:
-				ack.set_sequence(1) #nack
+				if sequence == data_link.__ACK_ONE:
+					ack.set_sequence(data_link.__NACK_ONE) #nack
+				else:
+					ack.set_sequence(data_link.__NACK_TWO) #nack
 				ack_bitstring = bin(ack.get_frame()).lstrip("0b")
 				lenght_bin = len(ack_bitstring)
 				for i in range(0, lenght_bin, 8):
@@ -92,9 +150,9 @@ class data_link:
 				physical_send.close()
 
 				response = True # precisa esperar outro frame
-
-			ack.set_sequence(0) # apos enviar um nack voltar para ack
+			
+			ack.set_sequence(sequence) # apos enviar um nack voltar para ack
 			port_client += 1
 			port_serv += 1
 		
-		return data_frame.bin_to_str(frame_check.get_payload()) # l_sdu
+		return frame_check, port_serv, port_client
