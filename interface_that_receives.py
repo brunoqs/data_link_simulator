12451bc8d6d6472401bb37e2bc323c8fd203f7df
Vinicius Spinelli <viinicius.sg@hotmail.com>
2018-07-15 12:33:32 -0300

    falta comentários e verificação código

--- interface_that_receives.py
+++ interface_that_receives.py
@@ -1,19 +1,14 @@
 import ipaddress
+import csv
 from link_layer.data_link import data_link
+
+def manegement_module():
+    with open('request_log.csv') as csvfile:
+        for reader in csv.reader(csvfile):
+            print(reader, "\n")
 
 data = data_link()
 destination_address = str(input())
 l_sdu = str(input())
 data.Data_Request(ipaddress.ip_address(destination_address), l_sdu)
-
-
-
-
-#Montar as estatisticas com base nos dados do arquivo
-logFile = open('logFile.txt', 'r')
-result = logFile.readLines()
-result = [s.rstrip() for s in result]#tirando os fins de linha e os espaços de cada posição da lista
-result = [_str.split(' ') for _str in result]
-
-
- 
+manegement_module()
--- interface_that_sends.py
+++ interface_that_sends.py
@@ -1,14 +1,15 @@
 import ipaddress
+import csv
 from link_layer.data_link import data_link
 
-data = data_link()#Construtor a ver
+def manegement_module():
+    with open('indication_log.csv') as csvfile:
+        for reader in csv.reader(csvfile):
+            print(reader, "\n")
+
+data = data_link()
 source_address = str(input())
 destination_address = str(input())
 data.Data_Indication(ipaddress.ip_address(destination_address), #envia dado
                      ipaddress.ip_address(source_address))
-    
-
-
-
-
-    
+manegement_module()
